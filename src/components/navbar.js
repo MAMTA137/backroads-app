da12aff36f417b6e04814cac14958c583f03aeab
MAMTA137 <mamtasavita47@gmail.com>
2023-07-11 17:59:06 +0530

    page links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import navlogo from '../images/logo.svg';
-
+import {pageLinks} from '../data';
 const Navbar = () => {
     return (
         <nav className="navbar">
@@ -12,22 +12,18 @@ const Navbar = () => {
         </div>
         {/* <!-- left this comment on purpose --> */}
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link"> home </a>
+          {pageLinks.map((link)=>{
+            return(
+          <li key={link.id}>
+            <a href={link.href} className="nav-link">{link.text}</a>
           </li>
 
-          <li>
-            <a href="#about" className="nav-link"> about </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link"> services </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link"> tours</a>
-          </li>
+            )
+          })}
         </ul>
+         
+          
+
 
         <ul className="nav-icons">
           <li>
