4f5d89881440b9c9281400c20f9a50b499e47ba5
MAMTA137 <mamtasavita47@gmail.com>
2023-07-13 01:05:11 +0530

    service

--- src/components/services.js
+++ src/components/services.js
@@ -1,5 +1,5 @@
 import Title from './title';
-
+import {services} from '../data'
 
 const Services = () =>{
     return (
@@ -7,39 +7,21 @@ const Services = () =>{
       <Title title='Our' subtitle='Services'/>
    
     <div className="section-center services-center">
-      <article className="service">
-        <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
+      {services.map((service)=>{
+        const{id,icons,title,text}=service;
+return(
+      <article className="service" key={id}>
+        <span className="service-icon"><i className={icons}></i></span>
         <div className="service-info">
-          <h4 className="service-title">saving money</h4>
-          <p className="service-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Asperiores, officia.
-          </p>
+          <h4 className="service-title">{title}</h4>
+          <p className="service-text"> {text}</p>
         </div>
       </article>
+)
+      })}
 
-      <article className="service">
-        <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
-        <div className="service-info">
-          <h4 className="service-title">endless hiking</h4>
-          <p className="service-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Asperiores, officia.
-          </p>
-        </div>
-      </article>
 
-      <article className="service">
-        <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
-        <div className="service-info">
-          <h4 className="service-title">amazing comfort</h4>
-          <p className="service-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Asperiores, officia.
-          </p>
-        </div>
-      </article>
-    </div>
+     </div>
   </section>)
 }
 export default Services;
